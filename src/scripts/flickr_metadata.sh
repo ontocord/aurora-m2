4ae44c4a58ab8aafbd286dc1cb0afb13c025196c
MLRichter <matrichter@uos.de>
2024-07-14 11:38:42 -0400

    sharded dataset

--- src/scripts/flickr_metadata.sh
+++ src/scripts/flickr_metadata.sh
@@ -2,9 +2,9 @@
 #SBATCH --account EUHPC_E03_068
 #SBATCH -p dcgp
 #SBATCH --time 01:00:00     # format: HH:MM:SS
-#SBATCH -N 1                # 1 node
+#SBATCH -N 2                # 1 node
 #SBATCH --ntasks-per-node=8 # 4 tasks out of 32
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=16
 #SBATCH --mem=123000          # memory per node out of 494000MB (481GB)
 #SBATCH --job-name=flickr
 #SBATCH --output=flickr-%j-%t.out
