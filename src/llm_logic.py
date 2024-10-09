236576013cc18ed965c37498d1d3947160fa0cc3
Huu4Ontocord <ontocord@gmail.com>
2024-10-09 09:48:12 -0700

    Update llm_logic.py

--- src/llm_logic.py
+++ src/llm_logic.py
@@ -7,3 +7,11 @@ print (len(files))
 file = random.choice(files)
 print (file)
 json.load(open(file))
+
+
+#!git clone https://github.com/teacherpeterpan/Logic-LLM
+dat = json.load(open("/content/Logic-LLM/data/AR-LSAT/train.json"))
+print (len(dat))
+for d in dat:
+  #print (d.keys())
+  print (d['context']+"\n"+d['question']+"\n"+"\n".join(d['options'])+"\nAnswer: "+d['answer'])
